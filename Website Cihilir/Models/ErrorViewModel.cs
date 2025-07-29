0cb12e48945d4e416fd17349bc49142c6b9e6dda
M Ridhwan Yunaser <mochammadridhwan@gmail.com>
2025-07-29 14:47:37 +0700

    Initial commit

---
+++ Website Cihilir/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Website_Cihilir.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
